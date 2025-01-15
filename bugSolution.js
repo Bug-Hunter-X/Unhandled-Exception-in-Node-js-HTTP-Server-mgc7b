f29fa5c65406f86f55a56af256f6885b09c18694
BugHunter-X <askzenai@gmail.com>
2025-01-15 23:12:07 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,25 @@
+const http = require('http');
+
+const requestListener = (request, response) => {
+  response.setHeader('Content-Type', 'application/json');
+  try {
+    //Simulate an error
+    //const error = new Error('Something went wrong!');
+    //throw error;
+    response.writeHead(200);
+    const jsonData = {
+      message: 'Hello from Node.js!',
+    };
+    response.end(JSON.stringify(jsonData));
+  } catch (error) {
+    console.error('Error handling request:', error);
+    response.writeHead(500, { 'Content-Type': 'application/json' });
+    response.end(JSON.stringify({ error: 'Internal Server Error' }));
+  }
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(8080, () => {
+  console.log('Server is running on port 8080');
+});
